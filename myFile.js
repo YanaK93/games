b66e190a8e6748c88e1654ea5f8adae79ab7a339
YanaK93 <120381671+YanaK93@users.noreply.github.com>
2023-04-25 21:40:37 +0300

    upd

---
+++ myFile.js
@@ -0,0 +1,48 @@
+window.onload = function () {
+    init();
+};
+
+function init() {
+    alert('Hello players');
+    const name = prompt('Please enter your name');
+    const figure = prompt('Please select rock/scissors/paper');
+    if (figure != 'rock' && figure != 'scissors' && figure != 'paper') 
+    {
+        alert('check not passed')
+    };
+    var words = ['rock', 'paper', 'scissors'];
+    var word = words[Math.floor(Math.random() * words.length)];
+    alert('Program choosed ' + word);
+    if (figure == word) 
+    {
+        alert('draw');
+    }
+    else 
+    {
+        if (figure == 'rock' && word == 'scissors') 
+        { 
+            alert('Player win') 
+        }
+        if (figure == 'rock' && word == 'paper')
+        {
+            alert('Computer win')
+        }
+        if (figure == 'scissors' && word == 'paper')
+        {
+            alert('Player win')
+        }
+        if (figure == 'scissors' && word == 'rock')
+        {
+            alert('Computer win')
+        }
+        if (figure == 'paper' && word == 'rock')
+        {
+            alert('Player win')
+        }
+        if (figure == 'paper' && word == 'scissors')
+        {
+            alert('Computer win')
+        }
+    }
+
+}
